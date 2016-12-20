227b95ccff44376e36e2df1adb9dd093584102a5
Raphael Courivaud <raphael.courivaud@gmail.com>
2016-12-20 22:05:19 +0100

    init

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# escalade-test
+# Static Heroku node.js app with Basic Auth
+
+Boilerplate code for static web app hosted with node.js on Heroku.
+
+Default username/password is `USERNAME`/`PASSWORD`. (Change this in `server.js`.)
+
+## Create your Heroku dyno:
+
+```sh
+heroku create
+git push heroku master
+heroku ps:scale web=1
+heroku open
+```
+
+## Rename it to something appropiate
+
+```sh
+heroku apps:rename NEWNAME
+heroku open
+```
--- server.js
+++ server.js
@@ -1,55 +1,28 @@
-#!/usr/bin/env node
+var express = require('express')
+  , passport = require('passport')
+  , BasicStrategy = require('passport-http').BasicStrategy;
 
-var db_users = [
-  {
-    id:1,
-    firstName: 'Jean',
-    lastName: 'Dupond',
-    club: 'Collège de Paris',
-    bestTime: '01:22:05',
-  },
-  {
-    id:2,
-    firstName: 'Raphaël',
-    lastName: 'Courivaud',
-    club: 'Collège Emiles Combes',
-    bestTime: '02:19:35',
-  },
-  {
-    id:3,
-    firstName: 'Pimette',
-    lastName: 'La Coquette',
-    club: 'Collège Emiles Combes',
-    bestTime: '01:10:22',
+
+// Use the BasicStrategy within Passport.
+//   Strategies in Passport require a `verify` function, which accept
+//   credentials (in this case, a username and password), and invoke a callback
+//   with a user object.
+passport.use(new BasicStrategy({},
+  function(username, password, done) {
+    var success = (username === 'USERNAME' && password === 'PASSWORD');
+    done(null, success);
   }
-];
+));
+
+var app = express();
+
+app.use(passport.initialize());
+// comment/remove this line below to disable auth
+app.use(passport.authenticate('basic', { session: false }));
+app.use(express.static(__dirname + '/public'));;
 
 
-/* Express 3 requires that you instantiate a `http.Server` to attach socket.io to first */
-var express = require('express'),
-    app = express(),
-    server = require('http').createServer(app),
-    io = require('socket.io').listen(server),
-    port = process.env.PORT || 8887,
-    url  = 'http://localhost:' + port + '/';
-
-server.listen(port);
-console.log("Express server listening on port " + port);
-console.log(url);
-
-app.use('/static', express.static(__dirname + '/static'));
-
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html');
-});
-
-//Socket.io emits this event when a connection is made.
-io.sockets.on('connection', function (socket) {
-
-    io.sockets.emit('users', db_users);
-    
-    socket.on('add-user', function (user) {
-      db_users.push(user);
-      io.sockets.emit('users', db_users);
-    });  
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function() {
+  console.log("Listening on " + port);
 });
